52108aab0c1db8e3d51a95c0dd1d29322659bb01
376811562@qq.com <kongwenkang@htmimi.com>
2018-10-15 16:55:22 +0800

    add -> login demo code

---
+++ example/demo/login-deomo/src/store/modules/auth.js
@@ -0,0 +1,77 @@
+import cookies from 'js-cookie'
+import Vue from 'vue'
+import { Message } from 'element-ui'
+Vue.prototype.$message = Message
+const state = () => ({
+  username: '',
+  token: ''
+})
+
+const showMessage = function (message, type, showClose) {
+  Vue.prototype.$message({
+    showClose: showClose,
+    type: type,
+    message: message
+  })
+}
+const actions = {
+  async login (
+    {
+      commit,
+      state,
+      dispatch
+    },
+    config
+  ) {
+    const { username, password } = config
+    if (username.length === 0 || password.length === 0) {
+      showMessage('账号或密码为空', 'warning', true)
+    } else {
+      const confirmUsername = 'kongwk'
+      const confirmPassword = '123456'
+      if (username === confirmUsername && password === confirmPassword) {
+        cookies.set('token', username)
+        cookies.set('username', username)
+        await commit('setUsername', username)
+        await commit('setToken', username)
+        return true
+      } else {
+        showMessage('账号或密码不正确', 'error', true)
+      }
+    }
+  }
+}
+
+const mutations = {
+  'setUsername' (state, username) {
+    state.username = username
+  },
+  'setToken' (state, token) {
+    state.token = token
+  }
+}
+
+const getters = {
+  'getUserName' (state) {
+    if (state.username) {
+      return state.username
+    } else {
+      return cookies.get('username')
+    }
+  },
+  'getToken' (state) {
+    if (state.token) {
+      return state.token
+    } else {
+      return cookies.get('token')
+    }
+  }
+}
+
+export default {
+  namespaced: true,
+  actions,
+  state,
+  mutations,
+  getters
+}
